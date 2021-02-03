2109e081f769eca9aeb3ae1c86a67be40ca954ad
Nidhal <nidhal.sghairi@goshaba.com>
2021-02-03 11:36:02 +0100

    version 2 with react-virtualized

--- src/App.js
+++ src/App.js
@@ -1,91 +1,105 @@
-
 import React from 'react'
-import initialData from './drivers.json'
-import Table from './components/Table'
+import { Column, Table, SortDirection, AutoSizer } from 'react-virtualized'
+import 'react-virtualized/styles.css'
+import _ from 'lodash'
+import data from './drivers.json'
 import styled from 'styled-components'
-
-const AppWrapper = styled.div`
-  align-items: center;
-  width: 100%;
-  margin: 60px 0;
-  display: block;
-`
 
 const Filter = styled.div`
   display: inline;
- 
 `
 
 const Input = styled.input`
   width: 100px;
   height: 20px;
 `
-const App = () => {
-  /** 
-   * the table data 
-   * */
-  const [data, setData] = React.useState(initialData)
 
-  /** 
-   * columnName represent the name of the column in which we apply filter
-   */
-  const [columnName, setColumnName] = React.useState(null)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-  /**
-   * the filter value
-   */
-  const [columnValue, setColumnValue] = React.useState(null)
+    const sortBy = 'NUT'    // the column used to sort data
+    const sortDirection = SortDirection.ASC   // sort direction (asc or desc)
+    const sortedList = this._sortList({ sortBy, sortDirection })
 
-  /**
-   * sortData is a function used to sort data by a given column
-   * @param {string} columnName the column by which we sort the data
-   */
-  const sortData = (columnName) => {
-    let sortedData = data.sort((a, b) =>
-      a[columnName] > b[columnName] ? -1 : 1
-    )
-    setData([...sortedData])
-  }
-
-  const handleSelectChange = (e) => {
-    setColumnName(e.target.value)
-  }
-
-  const handleInputChange = (e) => {
-    setColumnValue(e.target.value)
-  }
-
-
-  React.useEffect(() => {
-    if (columnValue && columnValue) {
-      let filteredData = initialData.filter(
-        (row) => row[columnName] == columnValue
-      )
-      setData([...filteredData])
+    this.state = {
+      filterName: null,
+      filterValue: null,
+      sortBy,
+      sortDirection,
+      sortedList,
     }
-  }, [columnName, columnValue])
+  }
+  _sortList = ({ sortBy, sortDirection }) => {
+    let newList = _.sortBy(data, [sortBy])
+    if (sortDirection === SortDirection.DESC) {
+      newList.reverse()
+    }
+    return newList
+  }
 
+  _sort = ({ sortBy, sortDirection }) => {
+    const sortedList = this._sortList({ sortBy, sortDirection })
+    this.setState({ sortBy, sortDirection, sortedList })
+  }
+  handleSelectChange = (e) => {
+    this.setState({ filterName: e.target.value })
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ filterValue: e.target.value })
+  }
+
+  render() {
   
-  return (
-    <AppWrapper>
-      <Filter>
-        <label>Choose a column to filter with : </label>
-        <select value={columnName} onChange={handleSelectChange}>
-          {Object.keys(initialData[0]).map((columnName) => {
-            return <option value={columnName}>{columnName}</option>
-          })}
-        </select>
+    return (
+      <div style={{ height: 400 }}>
+        <Filter>
+          <label>Choose a column to filter with : </label>
+          <select
+            value={this.state.filterName}
+            onChange={this.handleSelectChange}
+          >
+            {Object.keys(data[0]).map((columnName) => {
+              return <option value={columnName}>{columnName}</option>
+            })}
+          </select>
 
-        <label style={{marginLeft:'20px'}}>Choose a value : </label>
-        <Input
-          value={columnValue}
-          onChange={handleInputChange}
-        />
-      </Filter>
-
-      <Table data={data} sortData={sortData} />
-    </AppWrapper>
-  )
+          <label style={{ marginLeft: '20px' }}>Choose a value : </label>
+          <Input
+            value={this.state.filterValue}
+            onChange={this.handleInputChange}
+          />
+        </Filter>
+    
+        <AutoSizer>
+          {() => (
+            <Table
+              width={20000}
+              height={1000}
+              headerHeight={60}
+              rowHeight={30}
+              sort={this._sort}
+              sortBy={this.state.sortBy}
+              sortDirection={this.state.sortDirection}
+              rowCount={data.length}
+              rowGetter={({ index }) => 
+                this.state.sortedList[index][this.state.filterName] ==
+                this.state.filterValue
+                  ? this.state.sortedList[index]
+                  : ''
+              }
+            >
+              {Object.keys(data[0]).map((column) => {
+                return <Column label={column} dataKey={column} width={200} />
+              })}
+            </Table>
+          )}
+        </AutoSizer>
+        
+      </div>
+    )
+  }
 }
 
 export default App
